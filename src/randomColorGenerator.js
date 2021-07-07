cebaa3d750e2f58016a078fc50f0f4c029c1bfa0
Harold Torres Marino <haroldtm55@gmail.com>
2021-07-07 18:35:17 +1000

    Lab completed

--- src/randomColorGenerator.js
+++ src/randomColorGenerator.js
@@ -1,5 +1,5 @@
 // Nothing in this file needs to be altered (but it is your solution so feel free to!)
-export function getRandomColor() {
+export default function getRandomColor() {
   // this function generates a random hex color from the letters below
   const letters = '123456789AB' // <-- cutting off top end so we can lighten the color twice with 'reduceColor'
   let color = '#'
